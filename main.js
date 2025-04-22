8277775f1d95867919c84a1f14cfc56b3e08bb37
kevinmj12 <kevinmj12@gmail.com>
2025-04-22 19:51:05 +0900

    연동 및 /안녕 구현 완료

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
-const reminderFeature = require('./features/reminder');
+require("dotenv").config();
+const { App } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 });
 
-reminderFeature.init(app);
+app.command("/안녕", async ({ ack, say }) => {
+  await ack();
+  await say("안녕하세요!");
+});
 
 (async () => {
   await app.start();
-  console.log('⚡️ Slack 봇이 실행 중입니다!');
+  console.log("⚡️ Slack 봇이 실행 중입니다!");
 })();
