af4e2558fda7ad9ea631341f1080f5604fd96a77
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-05-03 10:40:39 +0900

    충돌 해결

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
+
 const reminderFeature = require('./features/reminder');
 const scheduleFeature = require('./features/schedule');
+const algoBotFeature = require('./features/algoBot'); 
+const {
+  studyService,
+  submitAnswerService,
+} = require("./features/study/study-service.js");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 });
 
 reminderFeature.init(app);
 scheduleFeature.init(app);
 
+// Algobot 알고리즘 문제 추천 기능
+algoBotFeature.init(app);
+
+// 면접 예상 질문 및 답변 기능
+app.command("/study", studyService);
+app.action("submit_answer_button", submitAnswerService);
+
 (async () => {
   await app.start();
-  console.log('⚡️ Slack 봇이 실행 중입니다!');
+  console.log("⚡️ Slack 봇이 실행 중입니다!");
 })();
